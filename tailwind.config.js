1d18a14ca96a3bc577a2f2dbd8b8ceae7c27263c
ohansyah <ohansyah@weekendinc.com>
2025-08-19 08:45:12 +0700

    fix: lighgt theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -47,5 +48,13 @@ export default {
                     'scrollbar-width': 'none',
                 },
             });
-        },],
+        },
+    ],
+
+    darkMode: 'class',
+    daisyui: {
+        themes: ["light"],
+        base: true,
+        styled: true,
+    },
 };
