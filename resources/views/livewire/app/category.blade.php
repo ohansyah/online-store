456f6377ad186341d4e26bd679db7cb784b0a01d
ohansyah <ohansyah@weekendinc.com>
2025-08-20 06:49:40 +0700

    adjust: color category list

--- resources/views/livewire/app/category.blade.php
+++ resources/views/livewire/app/category.blade.php
@@ -6,7 +6,7 @@
 
     <div class="group overflow-x-auto flex gap-2 no-scrollbar">
         @foreach ( $categories as $category)
-            <div class="w-24 h-24 flex-shrink-0 flex flex-col gap-2 p-2 rounded-xl">
+            <div class="w-24 h-24 flex-shrink-0 flex flex-col gap-2 p-2 rounded-xl border-2 border-white hover:border-brand-lighter transition duration-200 ease-out cursor-pointer">
                 <div class="relative rounded-xl overflow-hidden">
                     <img src="{{ $category['image_url'] }}" alt="{{ $category['name'] }}" class="w-full h-full object-cover" />
                 </div>
