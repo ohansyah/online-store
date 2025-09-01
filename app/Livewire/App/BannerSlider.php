8fad6c4a37b039cff1f2ebcc36e548d406371b67
ohansyah <ohan.ohansyah@gmail.com>
2025-09-01 09:57:44 +0700

    adjust: renaming file

--- app/Livewire/App/BannerSlider.php
+++ app/Livewire/App/BannerSlider.php
@@ -6,7 +6,7 @@ use App\Models\Banner as BannerModel;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
 
-class Banner extends Component
+class BannerSlider extends Component
 {
     public $banners;
     public function render()
@@ -15,6 +15,6 @@ class Banner extends Component
             return BannerModel::select('id', 'name', 'image')->active()->take(5)->get();
         });
 
-        return view('livewire.app.banner');
+        return view('livewire.app.banner-slider');
     }
 }
